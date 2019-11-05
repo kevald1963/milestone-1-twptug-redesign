e44e488c7e2e77caf3e6ab368db4d5788571b520
kevald1963 <kevin_alderson@yahoo.co.uk>
2019-11-05 16:40:41 +0000

    Moved about.html file to template folder, and adapted to inherit from base.html file.

--- run.py
+++ run.py
@@ -10,6 +10,10 @@ app.secret_key = 'some_secret_key'
 def index():
     return render_template("index.html")
 
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=os.environ.get('PORT'),
